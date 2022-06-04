6097897ef8a9e138bb6a11eba073f03efd27c5fb
Marco Lopez <lopez.marco@live.fr>
2022-06-04 14:14:51 +0200

    API et ajout du qa model

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,7 @@
+from webbrowser import get
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from harvai.qa_model import get_answer
 
 app = FastAPI()
 
@@ -15,20 +17,10 @@ app.add_middleware(
 def index():
     return {"Greeting": "Welcome to : HavAI - API"}
 
-@app.get("/classifier")
-def classifier(question):
-    # from the user input (question) get relative to one or mutliple articles
-
-    articles = "Please insert here articles"
-
-    return {"question": question,
-            "articles": articles }
-
 @app.get("/answer")
 def answer(question, articles=1):
     # from the user input (question) and articles, get the answer from hugging face
 
-    answer = "Please insert here answer"
+    answer = get_answer(question)['answer']
 
-    return {"question": question,
-            "answer": answer }
+    return {"question": question, "answer": answer }
