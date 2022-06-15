35a2c92bb61037d6a7eb47283b49499d55e8d31b
Olivier LePestipon <o.lepestipon@gmail.com>
2022-06-15 15:06:56 +0200

    embedding update

--- harvai/embedding.py
+++ harvai/embedding.py
@@ -1,6 +1,8 @@
 from harvai.data import get_clean_preproc_data
-from haystack.document_stores import ElasticsearchDocumentStore, InMemoryDocumentStore
+
 from haystack.nodes import EmbeddingRetriever
+from haystack.document_stores.faiss import FAISSDocumentStore
+from haystack.pipelines import DocumentSearchPipeline
 
 
 class Embedding():
@@ -21,25 +23,22 @@ class Embedding():
         for key,text in df.items():
             formatted_data.append({'id':key,'content':text['article_lowered']})
 
-        self.document_store = InMemoryDocumentStore(similarity="dot_product",embedding_dim=768)
-
-
-        #self.document_store = ElasticsearchDocumentStore(host="localhost", port="9200", username="", password="", index="document", similarity="cosine", embedding_dim=768 )
-        self.document_store.write_documents(formatted_data)
+        self.document_store = FAISSDocumentStore.load(index_path="haystack_data/embedding/testfile_path")
 
 
 
     def fit(self):
-        self.model = EmbeddingRetriever( document_store=self.document_store,
-        embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1",
+        self.model = EmbeddingRetriever(document_store=self.document_store,
+        embedding_model="sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
         model_format="sentence_transformers",
         use_gpu = False,
         )
-        self.document_store.update_embeddings(self.model)
 
     def predict(self,question):
-        candidate_documents = self.model.retrieve(query=question)
-        self.articles = [int(candidate_documents[id].id) for id in range(0,10)]
+        p_retrieval = DocumentSearchPipeline(self.model)
+        candidate_documents = p_retrieval.run(query=question, params={"Retriever": {"top_k": 10}})
+        self.articles = [int(candidate_documents['documents'][id].id) for id in range(0,10)]
+
 
     def get_articles_text_only (self, article_number=1):
         return ''.join(self.data.article_lowered[self.articles])
@@ -49,7 +48,8 @@ if __name__ == "__main__":
 
     test = Embedding()
     test.clean_data()
+
     test.fit()
     test.predict("quelle est la vitesse maximum sur l autoroute ?")
     print(test.articles)
-    #print(test.get_articles_text_only())
+    print(test.get_articles_text_only())
